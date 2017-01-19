67963244c893a91b300c0bc791a1eba2d811361e
fei.xiao <fei.xiao@yungoal.com>
2017-01-19 09:56:05 +0800

    add nlog message handler

---
+++ NLog.Extensions.AzureTableStorage/MessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NLog.Extensions.AzureTableStorage
+{
+    public abstract class MessageHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var corrId = Guid.NewGuid().ToString();
+
+            var requestMessage = await request.Content.ReadAsByteArrayAsync();
+
+            await this.IncomingMessageAsync(corrId, request, requestMessage);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            byte[] responseMessage;
+
+            if (response.IsSuccessStatusCode)
+            {
+                responseMessage = await response.Content.ReadAsByteArrayAsync();
+            }
+            else
+            {
+                responseMessage = Encoding.UTF8.GetBytes(response.ReasonPhrase);
+            }
+
+            stopwatch.Stop();
+            await this.OutgoingMessageAsync(corrId, response, responseMessage, stopwatch.ElapsedMilliseconds);
+
+            return response;
+        }
+
+        protected abstract Task IncomingMessageAsync(string correlationId, HttpRequestMessage request, byte[] message);
+
+        protected abstract Task OutgoingMessageAsync(string correlationId, HttpResponseMessage response, byte[] message, long elapsedMilliseconds);
+    }
+}
---
+++ NLog.Extensions.AzureTableStorage/NLogHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Extensions.AzureTableStorage
+{
+    public class NLogHandler : MessageHandler
+    {
+        private readonly Logger logger;
+
+        public NLogHandler() : this(null)
+        {
+        }
+
+        public NLogHandler(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "instrumentation";
+            }
+
+            logger = LogManager.GetLogger(name);
+        }
+
+        protected override Task IncomingMessageAsync(string correlationId, HttpRequestMessage request, byte[] message)
+        {
+            LogEventInfo eventInfo = new LogEventInfo();
+            eventInfo.Level = LogLevel.Trace;
+            eventInfo.LoggerName = logger.Name;
+            eventInfo.Properties.Add("ActiveId", correlationId);
+            eventInfo.Properties.Add("Method", request.Method);
+            eventInfo.Properties.Add("Url", request.RequestUri);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(request.Method).Append(" ").Append(request.RequestUri);
+
+            try
+            {
+                string body = Encoding.UTF8.GetString(message);
+                sb.Append(" Body: ").Append(body);
+            }
+            catch (Exception)
+            {
+                // ignore
+                sb.Append(" Can't parse body ");
+            }
+
+            eventInfo.Message = sb.ToString();
+            logger.Log(eventInfo);
+
+            return Task.FromResult(0);
+        }
+
+
+        protected override Task OutgoingMessageAsync(string correlationId, HttpResponseMessage response, byte[] message, long elapsedMilliseconds)
+        {
+            LogEventInfo eventInfo = new LogEventInfo();
+            eventInfo.Level = LogLevel.Trace;
+            eventInfo.LoggerName = logger.Name;
+            eventInfo.Properties.Add("ActiveId", correlationId);
+            eventInfo.Properties.Add("Method", response.RequestMessage.Method);
+            eventInfo.Properties.Add("Url", response.RequestMessage.RequestUri);
+            eventInfo.Properties.Add("StatusCode", (int)response.StatusCode);
+            StringBuilder sb = new StringBuilder();
+            sb.Append(response.RequestMessage.Method).Append(" ").Append(response.RequestMessage.RequestUri).Append(" Code: ").Append((int)response.StatusCode);
+
+            try
+            {
+                string body = Encoding.UTF8.GetString(message);
+                sb.Append(" Body: ").Append(body);
+            }
+            catch (Exception)
+            {
+                // ignore
+                sb.Append(" Can't parse body ");
+            }
+
+            eventInfo.Message = sb.ToString();
+            logger.Log(eventInfo);
+
+            return Task.FromResult(0);
+        }
+    }
+}
